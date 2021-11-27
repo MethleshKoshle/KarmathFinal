8b86dd3934f6dfd9d75be4e4954aa52b7da53336
MethleshKoshle <miitbh1@gmail.com>
2021-11-27 17:08:56 +0530

    Testing changes

--- app/src/main/java/com/methleshkoshle/karmathfinal/constant/Conditionals.java
+++ app/src/main/java/com/methleshkoshle/karmathfinal/constant/Conditionals.java
@@ -10,27 +10,35 @@ import android.os.Build;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Conditionals extends AppCompatActivity {
-    public static boolean isInternetWorking(Context context) {
-        final ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    public static boolean isInternetWorking(Context context){
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        if (cm != null) {
-            if (Build.VERSION.SDK_INT < 23) {
-                final NetworkInfo ni = cm.getActiveNetworkInfo();
-
-                if (ni != null) {
-                    return (ni.isConnected() && (ni.getType() == ConnectivityManager.TYPE_WIFI || ni.getType() == ConnectivityManager.TYPE_MOBILE));
-                }
-            } else {
-                final Network n = cm.getActiveNetwork();
-
-                if (n != null) {
-                    final NetworkCapabilities nc = cm.getNetworkCapabilities(n);
-
-                    assert nc != null;
-                    return (nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI));
-                }
-            }
-        }
-        return false;
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
     }
+//    public static boolean isInternetWorking(Context context) {
+//        final ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+//
+//        if (cm != null) {
+//            if (Build.VERSION.SDK_INT < 23) {
+//                final NetworkInfo ni = cm.getActiveNetworkInfo();
+//
+//                if (ni != null) {
+//                    return (ni.isConnected() && (ni.getType() == ConnectivityManager.TYPE_WIFI || ni.getType() == ConnectivityManager.TYPE_MOBILE));
+//                }
+//            } else {
+//                final Network n = cm.getActiveNetwork();
+//
+//                if (n != null) {
+//                    final NetworkCapabilities nc = cm.getNetworkCapabilities(n);
+//
+//                    assert nc != null;
+//                    return (nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI));
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
